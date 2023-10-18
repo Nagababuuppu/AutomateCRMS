d6387899f8cc77c762ab87e66633fc5f5a60b3b3
Admin <Admin@DESKTOP-IRS3JOK>
2023-10-18 13:02:47 +0530

    login functionality with dataprovider annotation

--- src/main/java/com/PageObjects/Login_functionality.java
+++ src/main/java/com/PageObjects/Login_functionality.java
@@ -1,10 +1,14 @@
 package com.PageObjects;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+ 
 
+import com.Utils.Utils;
 import com.base.Testbase;
 
 public class Login_functionality extends Testbase {
@@ -12,8 +16,6 @@ public class Login_functionality extends Testbase {
 	public Login_functionality() throws Throwable {
 		super();
 	}
-
-
 
 	@FindBy (xpath="//input[@placeholder='example@gmail.com']")
 	WebElement username;
@@ -45,8 +47,19 @@ public class Login_functionality extends Testbase {
 	
 	return new Homepage();
 	
-		
+    
 	}
+
+
+
+	public  Homepage DologinExcel(String Username,String Password) {
+		 
+		username.sendKeys(Username);
+		password.sendKeys(Password);
+		loginbutton.click();
+		return  new Homepage();
+	}
+	
 	
 	
 	
--- src/test/java/com/testcases/LoginTest.java
+++ src/test/java/com/testcases/LoginTest.java
@@ -1,17 +1,21 @@
 package com.testcases;
 
+import java.io.IOException;
+
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.PageObjects.Login_functionality;
+import com.Utils.Utils;
 import com.base.Testbase;
 
 public class LoginTest extends Testbase {
 
 	Login_functionality lf;
-
+    Utils u;
 	public LoginTest() throws Throwable {
 		super();
 	}
@@ -23,7 +27,7 @@ public class LoginTest extends Testbase {
 		Initialization();
 
 		lf = new Login_functionality(driver);
-
+		 u=new Utils(driver); 
 	}
 
 	@Test
@@ -34,15 +38,28 @@ public class LoginTest extends Testbase {
 
 		String url = driver.getCurrentUrl();
 
-		Assert.assertEquals("http://empirehome.myprojectsonline.co.in/EmpireHome/", url);
+		Assert.assertEquals("http://empirehome.myprojectsonline.co.in/EmpireHome/Dashboard", url);
 
+	}
+	@Test(dataProvider="user",dataProviderClass=LoginTest.class)
+	 public void Verfiylogin(String Username,String Password)
+	 {
+		 lf.DologinExcel(Username,Password);
+		 String url = driver.getCurrentUrl();
+
+			Assert.assertEquals("http://empirehome.myprojectsonline.co.in/EmpireHome/Dashboard", url);
+	 }
+	@DataProvider(name="user")
+	public  Object[][] getdata() throws IOException
+	{
+		return u.getdatafromexcel("Sheet1");
 	}
 
 	@AfterMethod
 
 	public void teardown() {
 
-		//driver.close();
+		driver.close();
 
 	}
 
